bb64c5d471de63fc63d663da58c82b9851e29bf3
M.Amin Rayej <m.amin.rayej@gmail.com>
2022-08-01 00:35:54 +0430

    initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,22 @@ mod error;
 
 use error::BuilderError::*;
 
-use quote::quote;
+use proc_macro2::{Punct, Spacing, TokenStream};
+use quote::{quote, ToTokens, TokenStreamExt};
+use syn::spanned::Spanned;
 use syn::*;
 
-fn is_wrapped_in(ty: Type, wrapper: &str) -> Option<Type> {
+// # Returns
+// * `Some`: Containing the type inside `Option`. For example calling this function
+//           on `Option<T>` returns `Some(T)`.
+// * `None`: If the type is not option.
+fn is_option(ty: &Type) -> Option<Type> {
     if let Type::Path(TypePath {
         path: Path { segments, .. },
         ..
     }) = ty
     {
-        if segments[0].ident == wrapper {
+        if segments[0].ident == "Option" {
             return match &segments[0].arguments {
                 PathArguments::None => None,
                 PathArguments::Parenthesized(_) => None,
@@ -27,6 +33,76 @@ fn is_wrapped_in(ty: Type, wrapper: &str) -> Option<Type> {
     }
 
     None
+}
+
+#[derive(Clone)]
+enum GenParamName {
+    Type(Ident),
+    Lifetime(Lifetime),
+    Const(Ident),
+}
+
+impl ToTokens for GenParamName {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            GenParamName::Type(ty) => tokens.append(ty.clone()),
+            GenParamName::Lifetime(lt) => {
+                let mut apostrophe = Punct::new('\'', Spacing::Joint);
+                apostrophe.set_span(lt.apostrophe);
+                tokens.append(apostrophe);
+                lt.ident.to_tokens(tokens);
+            }
+            GenParamName::Const(c) => tokens.append(c.clone()),
+        }
+    }
+}
+
+fn gen_params_into_names(generics: &Generics) -> Vec<GenParamName> {
+    generics
+        .params
+        .iter()
+        .map(|param| match param {
+            GenericParam::Type(ty) => GenParamName::Type(ty.ident.clone()),
+            GenericParam::Lifetime(lt) => GenParamName::Lifetime(lt.lifetime.clone()),
+            GenericParam::Const(c) => GenParamName::Const(c.ident.clone()),
+        })
+        .collect()
+}
+
+fn split_generic_params_names(
+    generic_params: Vec<GenParamName>,
+) -> (Vec<GenParamName>, Vec<GenParamName>, Vec<GenParamName>) {
+    let mut lifetimes = vec![];
+    let mut consts = vec![];
+    let mut types = vec![];
+
+    for param in generic_params {
+        match param {
+            GenParamName::Lifetime(_) => lifetimes.push(param.clone()),
+            GenParamName::Const(_) => consts.push(param.clone()),
+            GenParamName::Type(_) => types.push(param.clone()),
+        }
+    }
+
+    (lifetimes, consts, types)
+}
+
+fn split_generic_params(
+    generic_params: Vec<GenericParam>,
+) -> (Vec<GenericParam>, Vec<GenericParam>, Vec<GenericParam>) {
+    let mut lifetimes = vec![];
+    let mut consts = vec![];
+    let mut types = vec![];
+
+    for param in generic_params {
+        match param {
+            GenericParam::Lifetime(_) => lifetimes.push(param.clone()),
+            GenericParam::Const(_) => consts.push(param.clone()),
+            GenericParam::Type(_) => types.push(param.clone()),
+        }
+    }
+
+    (lifetimes, consts, types)
 }
 
 #[proc_macro_derive(Builder)]
@@ -45,47 +121,171 @@ pub fn builder(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                     struct_ident.span(),
                 );
 
-                let mut builder_fields = vec![];
-                let mut builder_inits = vec![];
+                let mut required_fields = vec![];
+                let mut optional_fields = vec![];
 
-                // TODO: Generate const generics for the builder.
+                let mut b_fields = vec![];
+                let mut b_inits = vec![];
+                for field in fields.iter() {
+                    let field_ident = &field.ident;
+                    let field_ty = &field.ty;
+                    let inner_ty = is_option(field_ty);
+                    let is_option = inner_ty.is_some();
 
-                for field in fields {
-                    let field_ident = field.ident.clone();
-                    let field_type = field.ty.clone();
-                    let inner_type = is_wrapped_in(field_type.clone(), "Option");
-                    let is_optional = inner_type.is_some();
-
-                    // --- Prepare fields of the builder struct:
-                    // For each field that is not wrapped in `Option` in the original struct,
-                    // we wrap it in `Option`, and any field that is already wrapped in `Option`,
-                    // we leave it as is. For example:
-                    // name: String        --> name: Option<String>
-                    // age : Option<usize> --> age : Option<usize>
-                    // We do this so we don't double wrap a field in `Option`.
-                    let builder_field = if is_optional {
-                        quote! { #field_ident: #field_type }
+                    if is_option {
+                        optional_fields.push(field);
                     } else {
-                        quote! { #field_ident: std::option::Option<#field_type> }
-                    };
-                    builder_fields.push(builder_field);
+                        required_fields.push(field);
+                    }
 
-                    // --- Prepare builder initializers:
-                    // For each field in the original struct, we will create a `builder_field`
-                    // which we know in wrapped in `Option`. So we need to initialize it with
-                    // `None`.
-                    builder_inits.push(quote! { #field_ident: None });
+                    b_fields.push(if is_option {
+                        quote! { #field_ident: #field_ty }
+                    } else {
+                        quote! { #field_ident: ::std::option::Option<#field_ty> }
+                    });
+
+                    b_inits.push(quote! { #field_ident: None });
+                }
+
+                // Struct generic Parameters
+                let struct_gen_param_names = gen_params_into_names(&ast.generics);
+                let (st_lt_pn, st_ct_pn, st_ty_pn) =
+                    split_generic_params_names(struct_gen_param_names);
+
+                let struct_gen_params: Vec<_> = ast.generics.params.iter().cloned().collect();
+                let (st_lt_p, st_ct_p, st_ty_p) = split_generic_params(struct_gen_params);
+
+                // Builder generic parameters
+                let mut all_false = vec![]; // <false, false>
+                let mut all_true = vec![]; // <false, false>
+                let mut b_ct_pn = vec![]; // <P0, P1>
+                let mut b_ct_p = vec![]; // <const P0: bool, const P1: bool>
+                for (index, field) in required_fields.iter().enumerate() {
+                    // let field_ident = &field.ident;
+                    // let field_ty = &field.ty;
+                    let const_param_ident = Ident::new(&format!("P{}", index), field.span());
+
+                    all_false.push(quote! { false });
+                    all_true.push(quote! { true });
+                    b_ct_pn.push(quote! { #const_param_ident });
+                    b_ct_p.push(quote! { const #const_param_ident: bool });
+                }
+
+                // Self setters
+                let mut req_self_setters = vec![];
+                for req_fields in &required_fields {
+                    let field_ident = &req_fields.ident;
+                    req_self_setters.push(quote! { #field_ident: self.#field_ident });
+                }
+
+                let mut opt_self_setters = vec![];
+                for opt_field in &optional_fields {
+                    let field_ident = &opt_field.ident;
+                    opt_self_setters.push(quote! { #field_ident: self.#field_ident });
+                }
+
+                // Setters
+                // let mut req_setters = vec![];
+                let mut opt_setters = vec![];
+
+                for (index, opt_field) in optional_fields.iter().enumerate() {
+                    let field_ident = &opt_field.ident;
+                    let field_ty = &opt_field.ty;   
+                    let inner_ty = is_option(field_ty).unwrap();
+
+                    let before_self_setters = &opt_self_setters[..index];
+                    let after_self_setters = &opt_self_setters[index + 1..];
+
+                    opt_setters.push(
+                        quote! {
+                            pub fn #field_ident(self, #field_ident: #inner_ty) -> 
+                                #builder_ident<#(#st_lt_pn,)* #(#st_ct_pn,)* #(#b_ct_pn,)* #(#st_ty_pn,)*> 
+                            {
+                                #builder_ident {
+                                    #(#before_self_setters,)*
+                                    #field_ident: Some(#field_ident),
+                                    #(#after_self_setters,)*
+                                    #(#req_self_setters,)*
+                                }
+                            }
+                        }
+                    );
+                }
+
+                let mut req_setters = vec![];
+
+                for (index, req_field) in required_fields.iter().enumerate() {
+                    let field_ident = &req_field.ident;
+                    let field_ty = &req_field.ty;   
+
+                    let before_self_setters = &req_self_setters[..index];
+                    let after_self_setters = &req_self_setters[index + 1..];
+
+                    let before_pn = &b_ct_pn[..index];
+                    let after_pn = &b_ct_pn[index + 1..];
+
+                    req_setters.push(
+                        quote! {
+                            pub fn #field_ident(self, #field_ident: #field_ty) -> 
+                                #builder_ident<#(#st_lt_pn,)* #(#st_ct_pn,)* #(#before_pn,)* true, #(#after_pn,)* #(#st_ty_pn,)*> 
+                            {
+                                #builder_ident {
+                                    #(#before_self_setters,)*
+                                    #field_ident: Some(#field_ident),
+                                    #(#after_self_setters,)*
+                                    #(#opt_self_setters,)*
+                                }
+                            }
+                        }
+                    );
+                }
+
+                // Builders
+                let mut req_field_builders = vec![];
+                for req_field in required_fields {
+                    let field_ident = &req_field.ident;
+
+                    req_field_builders.push(quote! { #field_ident: self.#field_ident.unwrap_unchecked() });
+                }
+
+                let mut opt_field_builders = vec![];
+                for opt_field in optional_fields {
+                    let field_ident = &opt_field.ident;
+
+                    opt_field_builders.push(quote! { #field_ident: self.#field_ident });
                 }
 
                 quote! {
-                    pub struct #builder_ident #ty_generics #where_clause {
-                        #(#builder_fields),*
+                    pub struct #builder_ident<#(#st_lt_p,)* #(#st_ct_p,)* #(#b_ct_p,)* #(#st_ty_p,)*> #where_clause {
+                        #(#b_fields),*
                     }
 
                     impl #impl_generics #struct_ident #ty_generics #where_clause {
-                        pub fn builder() -> #builder_ident #ty_generics {
+                        pub fn builder() -> #builder_ident<#(#st_lt_pn,)* #(#st_ct_pn,)* #(#all_false,)* #(#st_ty_pn,)*> {
                             #builder_ident {
-                                #(#builder_inits),*
+                                #(#b_inits),*
+                            }
+                        }
+                    }
+
+                    impl<#(#st_lt_p,)* #(#st_ct_p,)* #(#b_ct_p,)* #(#st_ty_p,)*> 
+                        #builder_ident<#(#st_lt_pn,)* #(#st_ct_pn,)* #(#b_ct_pn,)* #(#st_ty_pn,)* > 
+                        #where_clause 
+                    {
+                        #(#opt_setters)* 
+                        #(#req_setters)* 
+                    }
+
+                    impl<#(#st_lt_p,)* #(#st_ct_p,)* #(#st_ty_p,)*> 
+                        #builder_ident<#(#st_lt_pn,)* #(#st_ct_pn,)* #(#all_true,)* #(#st_ty_pn,)* > 
+                        #where_clause 
+                    {
+                        fn build(self) -> #struct_ident #ty_generics {
+                            unsafe {
+                                #struct_ident {
+                                    #(#opt_field_builders,)*
+                                    #(#req_field_builders,)*
+                                }
                             }
                         }
                     }
@@ -100,4 +300,3 @@ pub fn builder(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         Data::Union(union_t) => UnionErr(union_t).into(),
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 #[derive(tidy_builder::Builder)]
-pub struct Config<T> where T: std::fmt::Debug {
-    value: T
+pub struct Config<'a, const N: usize, T: std::fmt::Display>
+where
+    T: std::fmt::Debug,
+{
+    value: &'a T,
+    value_2: &'a T,
+    optional: Option<String>,
+    optional_2: Option<String>,
 }
 
-fn main() {}
+fn main() {
+    let config: Config<'static, 0, &str> = Config::builder()
+        .value(&"amin")
+        .value(&"sahar")
+        .optional("opt1".to_string())
+        .optional_2("opt2".to_string())
+        .value_2(&"faezeh")
+        .build();
 
+    assert_eq!(config.value, &"sahar");
+    assert_eq!(config.value_2, &"faezeh");
+    assert_eq!(config.optional.unwrap(), "opt1".to_string());
+    assert_eq!(config.optional_2.unwrap(), "opt2".to_string());
+    // Config::builder().value(&"amin").value_2(&"faezeh").build();
+    // Config::builder().value(&"amin").value_2(&"faezeh").build();
+    // Config::builder().value(&"amin").value_2(&"faezeh").build();
+}
