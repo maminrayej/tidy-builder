9d072221ad7fe5fb947c3872f817be1c7ef26222
sahandevs <sahandevs@gmail.com>
2022-08-25 16:06:07 +0430

    use `on_unimplemented` for better error messages

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,5 +1,19 @@
+use std::process::Command;
+
 #[test]
 fn ui_tests() {
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/**/*.rs");
+    drop(t); // dropping the `TestCases`, runs the tests
+
+    assert!(std::env::set_current_dir("./tests/nightly_ui").is_ok());
+
+    let result = Command::new("cargo")
+        .args(["+nightly", "test"])
+        .spawn()
+        .unwrap()
+        .wait_with_output()
+        .unwrap()
+        .status;
+    assert!(result.success());
 }
